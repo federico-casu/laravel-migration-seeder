1f18b97dd0df8ae0c60297f4c8dd562d17c0bdd3
federico-casu <federico.casu1101@gmail.com>
2024-03-17 23:48:37 +0100

    feat: records filtered by a query and sent them to homePage through PageController, then printed them

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class PageController extends Controller
 {
     public function index() {
 
-        return view('pages.home');
+        $today_s_trains = Train::where('orario_di_partenza', 'like', date('Y-m-d', strtotime(today())) . '%')->get();
+
+        return view('pages.home', compact('today_s_trains'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts.app')
 
 @section('title', 'Home')
-    
+
 @section('content')
-    <h1>Ciao Mondo</h1>    
+    <h1>Treni di oggi</h1>
+
+    {{-- {{ dd( date( 'Y-m-d', strtotime($today_s_trains[0]->orario_di_partenza) ) ) }} --}}
+
+    {{-- {{ dd($today_s_trains) }} --}}
+
+    {{-- {{ dd(date('Y-m-d', strtotime(today()))) }} --}}
+
+    <ul>
+        @foreach ($today_s_trains as $train)
+            <li>
+                <ul>
+                    <li>Id: {{ $train->id }}</li>
+                    <li>Azienda: {{ $train->azienda }}</li>
+                    <li>Stazione di partenza: {{ $train->stazione_di_partenza }}</li>
+                    <li>Stazione di arrivo: {{ $train->stazione_di_arrivo }}</li>
+                    <li>Orario di partenza: {{ $train->orario_di_partenza }}</li>
+                    <li>Orario di arrivo: {{ $train->orario_di_arrivo }}</li>
+                    <li>Codice treno: {{ $train->codice_treno }}</li>
+                    <li>Numero carrozze: {{ $train->numero_carrozze }}</li>
+                    <li>In orario: {{ $train->in_orario ? 'IN ORARIO' : 'IN RITARDO' }}</li>
+                    <li>Cancellato: {{ $train->cancellato ? 'CANCELLATO' : 'NON CANCELLATO' }}</li>
+                </ul>
+                <br>
+            </li>
+        @endforeach
+    </ul>
 @endsection
